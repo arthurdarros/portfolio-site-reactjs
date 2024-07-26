66ef19a62424e651c08b765d1d358ee8372e1cce
Arthur Stein Darros <arthurdarros@gmail.com>
2024-07-26 17:28:06 -0300

    init project

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+export function App() {
+
+  return (
+    <div className="bg-">
+      <div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
